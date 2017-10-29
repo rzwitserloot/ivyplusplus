f15d1f1b110734b779540e7f1a4bdb0d0cc092d0
Reinier Zwitserloot <reinier@zwitserloot.com>
2017-10-29 23:37:30 +0100

    [trivial] copyright year bump and version bump.

--- src/com/zwitserloot/ivyplusplus/Version.java
+++ src/com/zwitserloot/ivyplusplus/Version.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2010-2014 Reinier Zwitserloot.
+ * Copyright © 2010-2017 Reinier Zwitserloot.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,7 @@ package com.zwitserloot.ivyplusplus;
 
 public class Version {
 	// ** CAREFUL ** - this class must always compile with 0 dependencies (it must not refer to any other sources or libraries).
-	private static final String VERSION = "1.18";
+	private static final String VERSION = "1.20";
 	
 	private Version() {
 		//Prevent instantiation
